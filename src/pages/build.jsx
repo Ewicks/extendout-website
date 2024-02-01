efd51b45dab49c2977643795a663da9c4ad8f0af
ethan wicks <ethanwicks@ethans-MacBook-Air.local>
2024-02-01 21:42:50 +0000

    Fix some padding and layout issues

--- src/pages/build.jsx
+++ src/pages/build.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import KitchenFour from "../assets/images/kitchen-four.jpeg";
-import { Link } from 'react-router-dom';
-
-
+import { Link } from "react-router-dom";
 
 function Build() {
   return (
@@ -10,65 +8,63 @@ function Build() {
       <div className="extra-padding">
         <div className="row">
           <div className="col">
-            <h1 className="my-4">Design & Planning</h1>
+            <h1 className="my-4">Build</h1>
           </div>
         </div>
-        <div className="row my-5">
-          <div className="col">
-            <img className="design-img" src={KitchenFour} alt="" />
-          </div>
-          <div className="col">
-            <div className="design-container">
-              <p>
-              If you already have existing and proposed
-              drawings for your project, we can provide you
-               with a detailed build cost proposal to include 
-               the shell costs and all of your chosen 
-               specifications.
-              </p>
-              <h2>Specifications you may want to include in your build:</h2>
-              <br />
-              <ul>
-                <li>Bespoke Joinery</li>
-                <li>Decorating</li>
-                <li>Electrical</li>
-                <li>Flooring</li>
-                <li>Kitchen</li>
-                <li>Internal doors</li>
-                <li>Landscaping / patio</li>
-                <li>Bathroom / sanitary ware</li>
-                <li>Tiling</li>
-                <li>Windows & doors (external)</li>
-              </ul>
-              <p>
-              At ShapeHouse, our mission is to facilitate
-               a seamless and refined construction experience
-               for our clients. To achieve this, we have
-                high-quality tradesmen from Bricklayers, Roofers, 
-                Electricians, Plumbers, Carpenters, Plasterers, 
-                Tilers, Joiners, Kitchen Fitters, Heating Engineers,
-                 and Decorators—our expertise knows no bounds.
-              </p>
-              <br />
-              <p>
-              Recognising the importance of clear and open
-               communication, we assign a dedicated team of
-                project guides to serve as your steadfast
-                 point of contact throughout the entire build. We
-                  take immense pride in our commitment to
-                   transparency, offering regular valuations and
-                    updates, ensuring that you remain well-informed
-                and engaged as your vision materialises.
-              </p>
-              <br />
-              <p>So, allow yourself to revel in anticipation,
-                 as we meticulously attend to every detail of
-                your extraordinary build.
+        <div className="container-fluid">
+          <div className="row gap-5 my-5">
+            <div className="col">
+              <img className="design-img" src={KitchenFour} alt="" />
+            </div>
+            <div className="col">
+              <div className="design-container">
+                <p>
+                  If you already have existing and proposed drawings for your
+                  project, we can provide you with a detailed build cost
+                  proposal to include the shell costs and all of your chosen
+                  specifications.
                 </p>
-                <Link className='contact-btn my-5' to='/contact'>
+                <h2>Specifications you may want to include in your build:</h2>
+                <br />
+                <ul>
+                  <li>Bespoke Joinery</li>
+                  <li>Decorating</li>
+                  <li>Electrical</li>
+                  <li>Flooring</li>
+                  <li>Kitchen</li>
+                  <li>Internal doors</li>
+                  <li>Landscaping / patio</li>
+                  <li>Bathroom / sanitary ware</li>
+                  <li>Tiling</li>
+                  <li>Windows & doors (external)</li>
+                </ul>
+                <p>
+                  At ShapeHouse, our mission is to facilitate a seamless and
+                  refined construction experience for our clients. To achieve
+                  this, we have high-quality tradesmen from Bricklayers,
+                  Roofers, Electricians, Plumbers, Carpenters, Plasterers,
+                  Tilers, Joiners, Kitchen Fitters, Heating Engineers, and
+                  Decorators—our expertise knows no bounds.
+                </p>
+                <br />
+                <p>
+                  Recognising the importance of clear and open communication, we
+                  assign a dedicated team of project guides to serve as your
+                  steadfast point of contact throughout the entire build. We
+                  take immense pride in our commitment to transparency, offering
+                  regular valuations and updates, ensuring that you remain
+                  well-informed and engaged as your vision materialises.
+                </p>
+                <br />
+                <p>
+                  So, allow yourself to revel in anticipation, as we
+                  meticulously attend to every detail of your extraordinary
+                  build.
+                </p>
+                <Link className="contact-btn my-5" to="/contact">
                   Contact Us
-
                 </Link>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/gallary.jsx
+++ src/pages/gallary.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import OpenKitchen from '../assets/images/open-kitchen.jpeg';
-import LightedKitchen from '../assets/images/lighted-kitchen.jpeg';
-import IndoorKitchen from '../assets/images/indoor-kitchen.jpeg';
-import OpenRoom from '../assets/images/open-room.jpeg';
-import Bath from '../assets/images/bath.jpeg';
-import IndoorKitchenTwo from '../assets/images/indoor-kitchen-two.jpeg';
-import KitchenThree from '../assets/images/kitchen-three.jpeg';
+import OpenKitchen from "../assets/images/open-kitchen.jpeg";
+import LightedKitchen from "../assets/images/lighted-kitchen.jpeg";
+import IndoorKitchen from "../assets/images/indoor-kitchen.jpeg";
+import OpenRoom from "../assets/images/open-room.jpeg";
+import Bath from "../assets/images/bath.jpeg";
+import IndoorKitchenTwo from "../assets/images/indoor-kitchen-two.jpeg";
+import KitchenThree from "../assets/images/kitchen-three.jpeg";
 
 function Gallary() {
   return (
@@ -16,78 +16,80 @@ function Gallary() {
             <h1 className="my-5 gallary-title">Gallary</h1>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="gallery-container">
-              <div className="gallery-item">
-                <img src={LightedKitchen} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <div className="gallery-container">
+                <div className="gallery-item">
+                  <img src={LightedKitchen} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={OpenKitchen} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={OpenKitchen} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={IndoorKitchen} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={IndoorKitchen} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={OpenRoom} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={OpenRoom} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={Bath} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={Bath} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={IndoorKitchenTwo} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={IndoorKitchenTwo} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
-              </div>
-              <div className="gallery-item">
-                <img src={KitchenThree} alt="" />
-                <p
-                  className="gallary-name-tag d-flex 
+                  >
+                    test
+                  </p>
+                </div>
+                <div className="gallery-item">
+                  <img src={KitchenThree} alt="" />
+                  <p
+                    className="gallary-name-tag d-flex 
                 justify-content-center 
                 align-items-center"
-                >
-                  test
-                </p>
+                  >
+                    test
+                  </p>
+                </div>
               </div>
             </div>
           </div>

b8f6ed588acaada309cdcfb5a25690dcc7140825
ethan wicks <ethanwicks@ethans-MacBook-Air.local>
2024-02-01 21:30:13 +0000

    Fix x scroll for home page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,50 @@
-import React from 'react'
+import React from "react";
 import { FaPhone } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
-import Logo from '../assets/images/logo.png';
-import { Link } from 'react-router-dom'
-
-
-
+import Logo from "../assets/images/logo.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
+      <div className="container-fluid">
         <div className="row mobile-footer">
-            <div className="col d-flex justify-content-center align-items-center flex-column my-5">
-                <div className="div">
-                    <div className="logo">
-                        <img className='logo-img' src={Logo} alt="" />
-                    </div>
-                    <div className="phone mt-3">
-                        <FaPhone />
-                        <p className='phone-number'>07958635590</p>
-                    </div>
-                    <div className="phone">
-                        <FaPhone />
-                        <p className='phone-number'>02084028879</p>
-                    </div>
-                    <div className="email">
-                        <MdEmail />
-                        <Link to='/contact' className='email-address'>
-                            graham@extendout.co.uk
-                        </Link>
-                    </div>
-                    <p className='my-4 mobile-text'>© 2024 ShapeHouse | FAQ Privacy Policy</p>
-                </div>
+          <div className="col d-flex justify-content-center align-items-center flex-column my-5">
+            <div className="div">
+              <div className="logo">
+                <img className="logo-img" src={Logo} alt="" />
+              </div>
+              <div className="phone mt-3">
+                <FaPhone />
+                <p className="phone-number">07958635590</p>
+              </div>
+              <div className="phone">
+                <FaPhone />
+                <p className="phone-number">02084028879</p>
+              </div>
+              <div className="email">
+                <MdEmail />
+                <Link to="/contact" className="email-address">
+                  graham@extendout.co.uk
+                </Link>
+              </div>
+              <p className="my-4 mobile-text">
+                © 2024 ShapeHouse | FAQ Privacy Policy
+              </p>
             </div>
-            <div className="col d-flex justify-content-center align-items-center flex-column">
-                <div className="social-icons">
-                    <FaInstagram />
-                    <FaFacebook />
-                </div>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <div className="social-icons">
+              <FaInstagram />
+              <FaFacebook />
             </div>
+          </div>
         </div>
-           
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,133 +1,149 @@
 import React from "react";
-import Kitchen from "../assets/images/kitchen.png";
 import Kitchentwo from "../assets/images/kitchentwo.png";
 import Homeimage from "../assets/images/homeimage.jpg";
-import Extensions from '../assets/images/extensions.jpeg';
-import Loft from '../assets/images/loft.jpeg';
-import Renovation from '../assets/images/renovations.jpeg';
-import Remodelling from '../assets/images/remodelling.jpeg';
+import Extensions from "../assets/images/extensions.jpeg";
+import Loft from "../assets/images/loft.jpeg";
+import Renovation from "../assets/images/renovations.jpeg";
+import Remodelling from "../assets/images/remodelling.jpeg";
 
 import "../../src/App.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home-page">
       <img className="home-img" src={Homeimage} alt="" />
       <div className="center">
-      
-        <Link to='/contact' className="btn orange-btn">Contact Us</Link>
+        <Link to="/contact" className="btn orange-btn">
+          Contact Us
+        </Link>
       </div>
-      <div className="row">
-      </div>
-      <div className="row">
-        <div className="col d-flex justify-content-center align-items-center p-0 flex-column">
-          <h1>Extensions</h1>
-          <p className="p-5">
-            ExtendOut is a dedicated Loft Conversion and Extension company with
-            passion for good design and an eye for detail. We set the bar high
-            when it comes to the level of craftsmanship and service we provide.
-          </p>
-        </div>
-        <div className="col p-0">
-          <div className="extension-example">
-            <img className="kitchen-img" src={Extensions} alt="" />
-            <div className="overlay-text">
-              <p>aasd</p>
-              <p> My super text.</p>
-              <p>asd asd asd asdads.</p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <h1>Extensions</h1>
+            <p className="p-5">
+              ExtendOut is a dedicated Loft Conversion and Extension company
+              with passion for good design and an eye for detail. We set the bar
+              high when it comes to the level of craftsmanship and service we
+              provide.
+            </p>
+          </div>
+          <div className="col">
+            <div className="extension-example">
+              <img className="kitchen-img" src={Extensions} alt="" />
+              <div className="overlay-text">
+                <p>aasd</p>
+                <p> My super text.</p>
+                <p>asd asd asd asdads.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col p-0">
-          <div className="extension-example">
-            <img className="kitchen-img" src={Loft} alt="" />
-            <div className="overlay-text">
-              <p>aasd</p>
-              <p> My super text.</p>
-              <p>asd asd asd asdads.</p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <div className="extension-example">
+              <img className="kitchen-img" src={Loft} alt="" />
+              <div className="overlay-text">
+                <p>aasd</p>
+                <p> My super text.</p>
+                <p>asd asd asd asdads.</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col d-flex justify-content-center align-items-center p-0 flex-column">
-          <h1>Loft Conversions</h1>
-          <p className="p-5">
-            ExtendOut is a dedicated Loft Conversion and Extension company with
-            passion for good design and an eye for detail. We set the bar high
-            when it comes to the level of craftsmanship and service we provide.
-          </p>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col d-flex justify-content-center align-items-center p-0 flex-column">
-          <h1>Renovation</h1>
-          <p className="p-5">
-            ExtendOut is a dedicated Loft Conversion and Extension company with
-            passion for good design and an eye for detail. We set the bar high
-            when it comes to the level of craftsmanship and service we provide.
-          </p>
-        </div>
-        <div className="col p-0">
-          <div className="extension-example">
-            <img className="kitchen-img" src={Renovation} alt="" />
-            <div className="overlay-text">
-              <p>aasd</p>
-              <p> My super text.</p>
-              <p>asd asd asd asdads.</p>
-            </div>
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <h1>Loft Conversions</h1>
+            <p className="p-5">
+              ExtendOut is a dedicated Loft Conversion and Extension company
+              with passion for good design and an eye for detail. We set the bar
+              high when it comes to the level of craftsmanship and service we
+              provide.
+            </p>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col p-0">
-          <div className="extension-example">
-            <img className="kitchen-img" src={Kitchentwo} alt="" />
-            <div className="overlay-text">
-              <p>no image</p>
-              <p> My super text.</p>
-              <p>asd asd asd asdads.</p>
-            </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <h1>Renovation</h1>
+            <p className="p-5">
+              ExtendOut is a dedicated Loft Conversion and Extension company
+              with passion for good design and an eye for detail. We set the bar
+              high when it comes to the level of craftsmanship and service we
+              provide.
+            </p>
           </div>
-        </div>
-        <div className="col d-flex justify-content-center align-items-center p-0 flex-column">
-          <h1>New Build</h1>
-          <p className="p-5">
-            ExtendOut is a dedicated Loft Conversion and Extension company with
-            passion for good design and an eye for detail. We set the bar high
-            when it comes to the level of craftsmanship and service we provide.
-          </p>
-        </div>
-      </div>
-      
-      <div className="row">
-        <div className="col d-flex justify-content-center align-items-center p-0 flex-column">
-          <h1>Remodelling</h1>
-          <p className="p-5">
-            ExtendOut is a dedicated Loft Conversion and Extension company with
-            passion for good design and an eye for detail. We set the bar high
-            when it comes to the level of craftsmanship and service we provide.
-          </p>
-        </div>
-        <div className="col p-0">
-          <div className="extension-example">
-            <img className="kitchen-img" src={Remodelling} alt="" />
-            <div className="overlay-text">
-              <p>aasd</p>
-              <p> My super text.</p>
-              <p>asd asd asd asdads.</p>
+          <div className="col">
+            <div className="extension-example">
+              <img className="kitchen-img" src={Renovation} alt="" />
+              <div className="overlay-text">
+                <p>aasd</p>
+                <p> My super text.</p>
+                <p>asd asd asd asdads.</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col d-flex justify-content-center contact-banner align-items-center">
-          <button className="btn white-btn">Contact Us</button>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <div className="extension-example">
+              <img className="kitchen-img" src={Kitchentwo} alt="" />
+              <div className="overlay-text">
+                <p>no image</p>
+                <p> My super text.</p>
+                <p>asd asd asd asdads.</p>
+              </div>
+            </div>
+          </div>
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <h1>New Build</h1>
+            <p className="p-5">
+              ExtendOut is a dedicated Loft Conversion and Extension company
+              with passion for good design and an eye for detail. We set the bar
+              high when it comes to the level of craftsmanship and service we
+              provide.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col d-flex justify-content-center align-items-center flex-column">
+            <h1>Remodelling</h1>
+            <p className="p-5">
+              ExtendOut is a dedicated Loft Conversion and Extension company
+              with passion for good design and an eye for detail. We set the bar
+              high when it comes to the level of craftsmanship and service we
+              provide.
+            </p>
+          </div>
+          <div className="col">
+            <div className="extension-example">
+              <img className="kitchen-img" src={Remodelling} alt="" />
+              <div className="overlay-text">
+                <p>aasd</p>
+                <p> My super text.</p>
+                <p>asd asd asd asdads.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row mt-5">
+          <div className="col contact-banner d-flex justify-content-center align-items-center">
+            <Link to='/contact' className="btn banner-btn">
+            Contact Us
+            </Link>
+          </div>
         </div>
       </div>
     </div>
-    
   );
 };
 

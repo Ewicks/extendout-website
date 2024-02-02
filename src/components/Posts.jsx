3373896473043778808f354b3068291f72ef805c
ethan wicks <ethanwicks@ethans-MacBook-Air.local>
2024-02-02 11:53:02 +0000

    Truncate card text for blogs

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+function truncateText(text, maxLength) {
+  return text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
+}
+
 function Posts({ posts, loading }) {
   if (loading) {
     return <h2>Loading...</h2>;
@@ -8,12 +12,12 @@ function Posts({ posts, loading }) {
   return (
     <div className='row row-cols-1 row-cols-md-3'>
       {posts.map((post, index) => (
-        <div className='col mb-4' key={index}>
-          <div className='card'>
+        <div className='col p-3 mb-4' key={index}>
+          <div className='card post-card'>
             <img className='card-img-top' src={post.image} alt={post.title} />
             <div className='card-body'>
               <h3 className='card-title'>{post.title}</h3>
-              <p className='card-text'>{post.text}</p>
+              <p className='card-text'>{truncateText(post.text, 350)}</p>
               {/* <a href="#" className='blog-btn'>Contact Us</a> */}
             </div>
           </div>
--- src/dummy.js
+++ src/dummy.js
@@ -98,8 +98,8 @@ export const blogData = () => {
       },
       {
         "image": Kitchen,
-        "title": "asdasd",
-        "author": "John Doe",
+        "title": "test",
+        "author": "test",
         "text": "As renovation experts, we have found that many homeowners with traditional cottages aspire to modernise their living space by opening up the downstairs area. While this can certainly create a more spacious and contemporary feel, we often caution our clients against removing the downstairs WC. The downstairs WC may seem like an insignificant space, but ",
         "date": "2024/02/01",
         "id": 3

f6961a64a3cf80b9595f09725b69aaef3f365427
ethan <ethanowicks9@gmail.com>
2024-02-10 14:16:39 +0000

    Remove unused img

--- src/pages/design.jsx
+++ src/pages/design.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Oven from "../assets/images/oven.jpeg";
-import Kitchen from "../assets/images/kitchen.png";
 import { Link } from "react-router-dom";
 
 function Design() {

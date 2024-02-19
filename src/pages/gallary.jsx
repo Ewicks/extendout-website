bb7936a8925de5d5f73f546acc66903c044302b2
ethan <ethanowicks9@gmail.com>
2024-02-19 20:01:53 +0000

    Adjust gallery section

--- src/pages/gallary.jsx
+++ src/pages/gallary.jsx
@@ -26,90 +26,10 @@ function Gallary() {
             </div>
           </div>
         </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              {/* <h1 className="my-5 gallary-title">Gallery</h1> */}
-              <div className="gallery-container">
-                <div className="gallery-item">
-                  <img
-                    className="img-fluid gallery-img"
-                    src={LightedKitchen}
-                    alt=""
-                  />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Putney
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={OpenKitchen} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Eltham
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={IndoorKitchen} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Staines
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={OpenRoom} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Esher
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={Bath} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Esher
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={IndoorKitchenTwo} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Putney
-                  </p>
-                </div>
-                <div className="gallery-item">
-                  <img src={KitchenThree} alt="" />
-                  <p
-                    className="gallary-name-tag d-flex 
-                justify-content-center 
-                align-items-center"
-                  >
-                    Kingston
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="container-fluid gallery-container">
+    
+              
+                 
+        <div class="container-fluid gallery-container m-0">
           <div class="tz-gallery">
             <div class="row">
               <div class="col-sm-12 col-md-4">
@@ -135,6 +55,41 @@ function Gallary() {
               <div class="col-sm-6 col-md-4">
                 <div class="lightbox">
                   <img src={OpenRoom} alt="Coast" />
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="lightbox">
+                  <img src={Bath} alt="Rails" />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-4">
+                <div class="lightbox">
+                  <img src={LightedKitchen} alt="Bridge" />
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="lightbox">
+                  <img src={OpenKitchen} alt="Park" />
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="lightbox">
+                  <img src={IndoorKitchen} alt="Tunnel" />
+                </div>
+              </div>
+              <div class="col-sm-12 col-md-8">
+                <div class="lightbox">
+                  <img src={KitchenThree} alt="Traffic" />
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="lightbox">
+                  <img src={OpenRoom} alt="Coast" />
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4">
+                <div class="lightbox">
+                  <img src={Bath} alt="Rails" />
                 </div>
               </div>
               <div class="col-sm-6 col-md-4">
